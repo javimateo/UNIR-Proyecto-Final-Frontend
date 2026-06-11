92f3f1a7a85a51757121476cc5794b9b12692cd5
Rafael <faliflower07@gmail.com>
2026-06-11 21:46:27 +0200

    fase 3 y definitiva

--- frontend/src/app/pages/login/login-form/login-form.component.ts
+++ frontend/src/app/pages/login/login-form/login-form.component.ts
@@ -4,54 +4,58 @@ import { Router } from '@angular/router';
 import { AuthServiceServices } from '../../../service/auth-service.services';
 import { firstValueFrom } from 'rxjs';
 
-
 @Component({
   selector: 'app-login-form',
   standalone: true,
   imports: [ReactiveFormsModule],
   templateUrl: './login-form.component.html',
-  styleUrls: ['./login-form.component.css']
+  styleUrls: ['./login-form.component.css'],
 })
 export class LoginFormComponent {
   private router = inject(Router);
-  private authService = inject(AuthServiceServices)
+  private authService = inject(AuthServiceServices);
   loginForm: FormGroup;
 
   constructor() {
     this.loginForm = new FormGroup({
-      
-      email: new FormControl('', [Validators.required, Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/)]),
-      password: new FormControl('', [Validators.required, Validators.minLength(8)])
+      email: new FormControl('', [
+        Validators.required,
+        Validators.pattern(/^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$/),
+      ]),
+      password: new FormControl('', [Validators.required, Validators.minLength(8)]),
     });
   }
 
   checkControl(controlName: string, errorName: string): boolean | undefined {
-    return this.loginForm.get(controlName)?.hasError(errorName) && this.loginForm.get(controlName)?.touched;
+    return (
+      this.loginForm.get(controlName)?.hasError(errorName) &&
+      this.loginForm.get(controlName)?.touched
+    );
   }
 
   async getDataForm() {
     if (this.loginForm.valid) {
       try {
-        const response = await firstValueFrom (this.authService.login(this.loginForm.value))
-
+        const response = await firstValueFrom(this.authService.login(this.loginForm.value));
         console.log('Datos de login enviados:', this.loginForm.value);
-        
-        const role = this.authService.getRole()
 
-        if(role == 'ADMIN'){
-          this.router.navigate(['/admin-dashboard'])
-        }else if (role === 'MODERATOR'){
-          this.router.navigate(['/mod-dashboard'])
-        }else{
-          this.router.navigate(['/home'])
+        await firstValueFrom(this.authService.getProfile());
+
+        const role = this.authService.getRole();
+        console.log('*** EL ROL DETECTADO ES: ***', role);
+
+        if (role == 'admin') {
+          this.router.navigate(['/admin-dashboard']);
+        } else if (role === 'moderator') {
+          this.router.navigate(['/mod-dashboard']);
+        } else {
+          this.router.navigate(['/home']);
         }
-        
-        this.router.navigate(['/home']);
       } catch (error) {
         console.error('Error en el login:', error);
       }
-    }else{
-      this.loginForm.markAllAsTouched()
+    } else {
+      this.loginForm.markAllAsTouched();
     }
   }
 }
