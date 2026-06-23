155eaa335c5355529bd05aedba59d5389bbeadc0
Rafael <faliflower07@gmail.com>
2026-06-23 20:00:07 +0200

    1ªfase admin

---
+++ frontend/src/app/pages/admin-dashboard/components/estadisticas-globales/estadisticas-globales.component.ts
@@ -0,0 +1,9 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-estadisticas-globales',
+  imports: [],
+  templateUrl: './estadisticas-globales.component.html',
+  styleUrl: './estadisticas-globales.component.css',
+})
+export class EstadisticasGlobalesComponent {}
