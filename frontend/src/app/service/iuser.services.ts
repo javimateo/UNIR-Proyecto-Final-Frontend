ca6ede9bf56455dc025106ce74210604f10177d3
Rafael <faliflower07@gmail.com>
2026-06-24 10:17:10 +0200

    modificación del service
    el servicio esta modificado para que sea reutilizable

--- frontend/src/app/service/iuser.services.ts
+++ frontend/src/app/service/iuser.services.ts
@@ -4,28 +4,31 @@ import { IUser, IUserListResponse } from '../interface/iuser.interface';
 import { lastValueFrom } from 'rxjs';
 
 @Injectable({
-  providedIn: 'root',
+  providedIn: 'root'
 })
 export class IUserServices {
   private httpClient = inject(HttpClient);
-  private baseUrl = 'http://localhost:3000/api/auth/register';
+  private baseUrl = 'http://localhost:3000/api'; 
 
-  getAllPromises(url: string= ''): Promise<IUserListResponse> {
-    const miUrl = (url === "") ? this.baseUrl : url;
+  
+  getAllPromises(url: string = ''): Promise<IUserListResponse> {
+    const miUrl = (url === "") ? `${this.baseUrl}/users` : url; 
     return lastValueFrom(this.httpClient.get<IUserListResponse>(miUrl));
-
-}
-  getById(id:number): Promise<IUser> {
-    return lastValueFrom(this.httpClient.get<IUser>(`${this.baseUrl}/${id}`));
   }
 
-   create(formValue: IUser): Promise<IUser> {
+  
+  create(formValue: IUser): Promise<IUser> {
     const userToSave: Partial<IUser> = {
       username: formValue.username,
       apellido: formValue.apellido,
       email: formValue.email,
       password: formValue.password
     };
-    return lastValueFrom(this.httpClient.post<IUser>(this.baseUrl, userToSave));
+    
+    return lastValueFrom(this.httpClient.post<IUser>(`${this.baseUrl}/auth/register`, userToSave));
+  }
+
+  getById(id: number): Promise<IUser> {
+    return lastValueFrom(this.httpClient.get<IUser>(`${this.baseUrl}/users/${id}`));
   }
 }
