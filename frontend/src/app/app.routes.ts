0381ad50cfb421c7fbb7724b66655eb8593fe214
Jonass <xtaxsitrolol@gmail.com>
2026-06-30 11:32:50 +0200

    feat: implementar panel de usuario privado con secciones de anuncios, favoritos, mensajería y perfil

--- frontend/src/app/app.routes.ts
+++ frontend/src/app/app.routes.ts
@@ -5,6 +5,7 @@ import { ItemDetailComponent } from './pages/item-detail/item-detail.component';
 import { ItemFormComponent } from './pages/item-form/item-form.component';
 import { AdminDashboardComponent } from './pages/admin-dashboard/admin-dashboard.component';
 import { ModDashboardComponent } from './pages/mod-dashboard/mod-dashboard.component';
+import { UserDashboardComponent } from './pages/user-dashboard/user-dashboard.component';
 import { adminGuard } from './guards/admin-guard';
 import { moderadorGuard } from './guards/moderador-guard';
 
@@ -15,6 +16,7 @@ export const routes: Routes = [
     {path:"anuncio/nuevo", component:ItemFormComponent},
     {path:"anuncio/editar/:id", component:ItemFormComponent},
     {path:"anuncio/:id", component:ItemDetailComponent},
+    {path:"dashboard", component:UserDashboardComponent},
     {path:"admin-dashboard", component:AdminDashboardComponent, canActivate:[adminGuard]},
     {path:"mod-dashboard", component:ModDashboardComponent, canActivate:[moderadorGuard]},
 ];
